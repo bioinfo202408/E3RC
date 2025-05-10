4dca3086ea55f32971b5da5fe70dfc6761f895b3
nculiujy <406400240012@email.ncu.edu.cn>
2025-05-10 15:48:58 +0800

    del

--- script/Figure3_scripts/list_of_eRNA_regulated_by_TF.R
+++
@@ -1,8 +0,0 @@
-args <- commandArgs(trailingOnly = TRUE)
-tfName <- args[1]
-stage <- args[2]
-data <- read.csv(file = args[3], header = FALSE)
-subset_data <- data[data[,2] == tfName & data[,9] == stage, ]
-sorted_data <- subset_data[order(subset_data[,10], decreasing = TRUE), ]
-sorted_data <- head(sorted_data, 10)
-write.csv(sorted_data,file=args[4],row.names=FALSE,col.names=FALSE,quote=FALSE)
