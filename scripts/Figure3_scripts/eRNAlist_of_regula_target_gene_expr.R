6530c85df964cac52d3deaf6f29e558170af0f17
nculiujy <406400240012@email.ncu.edu.cn>
2025-05-30 10:41:54 +0800

    1

--- scripts/Figure3_scripts/eRNAlist_of_regula_target_gene_expr.R
+++
@@ -1,8 +0,0 @@
-args<-commandArgs(trailingOnly = TRUE)
-geneID<-args[1]
-stage<-args[2]
-data<-read.csv(args[3],head = TRUE)
-subset_data<-data[data[,3]==geneID & data[,8]==stage,]
-sorted_data<-subset_data[order(subset_data[,6],decreasing = TRUE),]
-sorted_data <- head(sorted_data, 10)
-write.csv(sorted_data,file=args[4],row.names=FALSE,quote=FALSE)
